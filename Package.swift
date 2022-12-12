8fb165ae1b8d40bae9e6325ad5f564acae2287ee
Frizlab <fload@me.com>
2022-12-12 22:39:39 +0100

    Use variables in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,20 @@ import PackageDescription
 
 let package = Package(
 	name: "BMO",
-	products: [
-		.library(name: "BMO", targets: ["BMO"])
-	],
-	dependencies: [
-//		.package(url: "https://github.com/happn-app/CollectionLoader.git", .branch("dev.tests"))
-	],
-	targets: [
-		.target(name: "BMO"),
-		.testTarget(name: "BMOTests", dependencies: ["BMO"])
-	]
+	products: {
+		var res = [Product]()
+		res.append(.library(name: "BMO", targets: ["BMO"]))
+		return res
+	}(),
+	dependencies: {
+		let res = [Package.Dependency]()
+//		res.append(.package(url: "https://github.com/happn-app/CollectionLoader.git", branch: "dev.tests"))
+		return res
+	}(),
+	targets: {
+		var res = [Target]()
+		res.append(.target(name: "BMO"))
+		res.append(.testTarget(name: "BMOTests", dependencies: ["BMO"]))
+		return res
+	}()
 )
