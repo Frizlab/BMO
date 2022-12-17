66d9d832dcac5a6fc086bf4c52ccf7e17a7992a5
Frizlab <fload@me.com>
2022-12-17 21:27:44 +0100

    Use a better format for the Package.swift file

--- Package.swift
+++ Package.swift
@@ -4,24 +4,23 @@ import PackageDescription
 
 let package = Package(
 	name: "BMO",
-	products: {
-		var res = [Product]()
-		res.append(.library(name: "BMO", targets: ["BMO"]))
-		return res
-	}(),
-	dependencies: {
-		var res = [Package.Dependency]()
-		res.append(.package(url: "https://github.com/Frizlab/HasResult.git", from: "1.0.0"))
-		return res
-	}(),
-	targets: {
-		var res = [Target]()
-		res.append(.target(name: "BMO", dependencies: {
-			var res = [Target.Dependency]()
-			res.append(.product(name: "HasResult", package: "HasResult"))
-			return res
-		}()))
-		res.append(.testTarget(name: "BMOTests", dependencies: ["BMO"]))
-		return res
-	}()
+	products: buildArray{
+		$0.append(.library(name: "BMO", targets: ["BMO"]))
+	},
+	dependencies: buildArray{
+		$0.append(.package(url: "https://github.com/Frizlab/HasResult.git", from: "1.0.0"))
+	},
+	targets: buildArray{
+		$0.append(.target(name: "BMO", dependencies: buildArray{
+			$0.append(.product(name: "HasResult", package: "HasResult"))
+		}))
+		$0.append(.testTarget(name: "BMOTests", dependencies: ["BMO"]))
+	}
 )
+
+
+func buildArray<Element>(of type: Any.Type = Element.self, _ builder: (_ collection: inout [Element]) -> Void) -> [Element] {
+	var ret = [Element]()
+	builder(&ret)
+	return ret
+}
