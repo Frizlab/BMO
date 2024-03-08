a657a91d7a05058de485979f6da51dc6b1e529ff
Frizlab <fload@me.com>
2024-03-08 04:55:34 +0100

    Get rid of buildArray in the Package definition

--- Package.swift
+++ Package.swift
@@ -7,24 +7,17 @@ let swiftSettings: [SwiftSetting] = []
 
 let package = Package(
 	name: "BMO",
-	products: buildArray{
-		$0.append(.library(name: "BMO", targets: ["BMO"]))
-		$0.append(.library(name: "BMOCoreData", targets: ["BMOCoreData"]))
-	},
-	targets: buildArray{
-		$0.append(.target(name: "BMO", swiftSettings: swiftSettings))
-		$0.append(.testTarget(name: "BMOTests", dependencies: ["BMO"], swiftSettings: swiftSettings))
+	products: [
+		.library(name: "BMO", targets: ["BMO"]),
+		.library(name: "BMOCoreData", targets: ["BMOCoreData"]),
+	],
+	targets: [
+		.target(name: "BMO", swiftSettings: swiftSettings),
+		.testTarget(name: "BMOTests", dependencies: ["BMO"], swiftSettings: swiftSettings),
 		
-		$0.append(.target(name: "BMOCoreData", dependencies: buildArray{
-			$0.append(.target(name: "BMO"))
-		}, swiftSettings: swiftSettings))
-		$0.append(.testTarget(name: "BMOCoreDataTests", dependencies: ["BMOCoreData"], swiftSettings: swiftSettings))
-	}
+		.target(name: "BMOCoreData", dependencies: [
+			.target(name: "BMO"),
+		], swiftSettings: swiftSettings),
+		.testTarget(name: "BMOCoreDataTests", dependencies: ["BMOCoreData"], swiftSettings: swiftSettings),
+	]
 )
-
-
-func buildArray<Element>(of type: Any.Type = Element.self, _ builder: (_ collection: inout [Element]) -> Void) -> [Element] {
-	var ret = [Element]()
-	builder(&ret)
-	return ret
-}
