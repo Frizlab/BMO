4951a7b6d3db9615a060a3af009a4d08db5a8f13
Frizlab <fload@me.com>
2024-03-08 05:00:40 +0100

    Fix a Swift 5.10 Sendability warning

--- Sources/BMOCoreData/Utilities/Private/ Loggers.swift
+++ Sources/BMOCoreData/Utilities/Private/ Loggers.swift
@@ -3,21 +3,26 @@ import os.log
 
 
 
+#if swift(>=6.0)
+/* See <https://developer.apple.com/forums/thread/747816?answerId=781922022#781922022>. */
+#warning("Reevaluate whether nonisolated(unsafe) is still necessary.")
+#endif
+
 @available(macOS 11.0, tvOS 14.0, iOS 14.0, watchOS 7.0, *)
 extension Logger {
 	
-	static let importer          = {Logger(OSLog.importer)}()
-	static let saveRequestHelper = {Logger(OSLog.saveRequestHelper)}()
+	nonisolated(unsafe) static let importer          = {Logger(OSLog.importer)}()
+	nonisolated(unsafe) static let saveRequestHelper = {Logger(OSLog.saveRequestHelper)}()
 	
 }
 
 extension OSLog {
 	
-	static let importer = {
+	nonisolated(unsafe) static let importer = {
 		return OSLog(subsystem: OSLog.subsystem, category: "Importer")
 	}()
 	
-	static let saveRequestHelper = {
+	nonisolated(unsafe) static let saveRequestHelper = {
 		return OSLog(subsystem: OSLog.subsystem, category: "SaveRequestHelper")
 	}()
 	
