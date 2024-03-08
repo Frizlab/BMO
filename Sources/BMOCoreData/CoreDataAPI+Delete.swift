1136f494411950cc5b1548a34bc6de7562a5a01c
Frizlab <fload@me.com>
2024-03-08 05:25:23 +0100

    Fix CoreData being used outside of a context

--- Sources/BMOCoreData/CoreDataAPI+Delete.swift
+++ Sources/BMOCoreData/CoreDataAPI+Delete.swift
@@ -34,35 +34,37 @@ public extension CoreDataAPI {
 		let settings = settings ?? defaultSettings
 		let requestUserInfo = requestUserInfo ?? defaultRequestUserInfo
 		
-		let object = try localDb.context.existingObject(with: objectID)
-		/* We do not need a temporary context like for update or create operation as we do not modify the context to send the request.
-		 * So we use the saveBeforeGoingRemote save workflow (nothing will be saved as there should be no modifications in the context). */
-		let bridgeRequest = settings.deleteObjectBridgeRequest(object, .saveBeforeGoingRemote)
-		let opRequest = Request(localDb: localDb, localRequest: bridgeRequest, remoteUserInfo: requestUserInfo)
-		let op = RequestOperation(bridge: bridge, request: opRequest, remoteOperationQueue: settings.remoteOperationQueue, computeOperationQueue: settings.computeOperationQueue, startedOnContext: false)
-		op.completionBlock = { /* We keep a strong ref to op but it’s not a problem because we nullify the completion block at the end of the block. */
-			DispatchQueue.main.async{
-				let result = op.result
-				if case .success = result {
-					/* If the request succeeded, we try and remove the Core Data object. */
-					localDb.context.perform{
-						localDb.context.delete(object)
-						if let error = localDb.context.saveOrRollback() {
-							handler(.failure(error))
-						} else {
-							handler(result.mapError{ $0 as Error })
+		return try localDb.context.performAndWaitRW{
+			let object = try localDb.context.existingObject(with: objectID)
+			/* We do not need a temporary context like for update or create operation as we do not modify the context to send the request.
+			 * So we use the saveBeforeGoingRemote save workflow (nothing will be saved as there should be no modifications in the context). */
+			let bridgeRequest = settings.deleteObjectBridgeRequest(object, .saveBeforeGoingRemote)
+			let opRequest = Request(localDb: localDb, localRequest: bridgeRequest, remoteUserInfo: requestUserInfo)
+			let op = RequestOperation(bridge: bridge, request: opRequest, remoteOperationQueue: settings.remoteOperationQueue, computeOperationQueue: settings.computeOperationQueue, startedOnContext: autoStart)
+			op.completionBlock = { /* We keep a strong ref to op but it’s not a problem because we nullify the completion block at the end of the block. */
+				DispatchQueue.main.async{
+					let result = op.result
+					if case .success = result {
+						/* If the request succeeded, we try and remove the Core Data object. */
+						localDb.context.perform{
+							localDb.context.delete(object)
+							if let error = localDb.context.saveOrRollback() {
+								handler(.failure(error))
+							} else {
+								handler(result.mapError{ $0 as Error })
+							}
 						}
+					} else {
+						handler(result.mapError{ $0 as Error })
 					}
-				} else {
-					handler(result.mapError{ $0 as Error })
 				}
+				op.completionBlock = nil /* In theory not needed anymore; I never tested that… */
 			}
-			op.completionBlock = nil /* In theory not needed anymore; I never tested that… */
+			if autoStart {
+				op.start() /* RequestOperations usually do not need to be queued at all: they mostly queue other operations and don’t do much on their own. */
+			}
+			return op
 		}
-		if autoStart {
-			op.start() /* RequestOperations usually do not need to be queued at all: they mostly queue other operations and don’t do much on their own. */
-		}
-		return op
 	}
 	
 	@discardableResult
@@ -79,7 +81,7 @@ public extension CoreDataAPI {
 					requestUserInfo: requestUserInfo,
 					settings: settings, autoStart: true,
 					handler: { res in
-						continuation.resume(with: res.mapError{ $0 as Error })
+						continuation.resume(with: res)
 					}
 				)
 			} catch {
